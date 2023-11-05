d4d43c7e5e13aac8bb3a47a3df010c845453b1bf
kimmingyo8 <kimmingyo8@naver.com>
2023-11-05 16:18:34 +0900

    마커 아이콘 지도에 띄우기

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 // global kakao
 
 import Script from 'next/script';
-// import * as stores from '@/data/store_data.json';
+import * as stores from '@/data/store_data.json';
 
 declare global {
   interface Window {
@@ -21,7 +21,36 @@ const Map = () => {
         center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
         level: 3,
       };
-      new window.kakao.maps.Map(mapContainer, mapOptions);
+      const map = new window.kakao.maps.Map(mapContainer, mapOptions);
+
+      // 마커 띄우기
+      stores?.['DATA']?.map((store) => {
+        const imageSrc = store?.bizcnd_code
+            ? `/images/markers/${store.bizcnd_code}.png`
+            : `/images/markers/default.png`,
+          imageSize = new window.kakao.maps.Size(40, 40),
+          imageOption = { offset: new window.kakao.maps.Point(27.69) };
+
+        const markerImage = new window.kakao.maps.MarkerImage(
+            imageSrc,
+            imageSize,
+            imageOption,
+          ),
+          // 마커 생성 위치
+          markerPosition = new window.kakao.maps.LatLng(
+            store?.y_dnts,
+            store?.x_cnts,
+          );
+
+        // 마커 생성
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+          image: markerImage,
+        });
+
+        // 마커 지도 위에 표시
+        marker.setMap(map);
+      });
     });
   };
 
